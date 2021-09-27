a570eb4162d40b0696cae9c19d3a445935d6e92c
YiQuan Zhao <hoshisakan@gmail.com>
2021-09-27 12:11:46 +0800

    2021-09-27-fourth-commit-modify-config-file-default-driver-path

--- instance/config.py
+++ instance/config.py
@@ -3,7 +3,7 @@ import os
 class Initialization:
     open_browser = True
     browser_type = 'chrome'
-    webdriver_path = "Driver_Path=D:\Files\Project\Crawler\driver\msedgedriver.exe"
+    webdriver_path = "D:\Files\Project\Crawler\driver\msedgedriver.exe"
     base_url_list = {"google_search": "https://www.google.com.tw/"}
     output_path = "D:\Files\Project\Google_Search_Crawler\output\csv"
     crawler_target_list = []
